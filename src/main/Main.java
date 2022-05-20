0a1f2ff21d5b3fc97b9a66dd1f18a70ebf7da2ac
Ashley Jensen <ashleyjensen@ashleys-imac.lan>
2022-05-20 17:10:05 -0500

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
